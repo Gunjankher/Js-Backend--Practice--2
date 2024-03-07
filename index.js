b99fea7a60ed6692df26bd73856aaf3478f18242
Gunjankher <gkhercode@gmail.com>
2024-03-07 10:47:10 +0530

    first express app

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Backend practice repo");
+console.log("this is Backend Practice Repo");
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get ('/twitter', (req,res)=>{
+    res.send('<Button>hello this is a <br/>twitter sign up </Button>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
